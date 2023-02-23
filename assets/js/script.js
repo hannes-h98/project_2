7f4abba8dfdc781ff37705c2768778399b20cca6
Hannes Hellmér <hhellmer@gmail.com>
2023-02-23 08:50:17 +0000

    Add Questions array to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// The quiz questions made as an array 
+const QUESTIONS = [
+    {
+    question: "wich movie has the highest imdb score?",
+    answers: {
+        a: "The Shawshank Redemption",
+        b: "The Pianist",
+        c: "Saving Private Ryan",
+        d: "The Shining"
+    },
+    rightanswer: "a"
+},
+{
+    question: "Wich one of these movies are not a Christopher Nolan movie",
+    answers: {
+        a: "The Dark Knight",
+        b: "Inception",
+        c: "Interstellar",
+        d: "Django Unchained"
+    },
+    rightanswer: "d"
+},
+{
+    question: "Who won the Oscar for best actor 2022",
+    answers: {
+        a: "Denzel Washington",
+        b: "Will Smith",
+        c: "Andrew Garfield",
+        d: "Benedict Cumberbatch"
+    },
+    rightanswer: "b"
+},
+{
+    question: "In wich of the first Harry Potter movies does Harry first encounter a dementor",
+    answers: {
+        a: "The first one",
+        b: "The second one",
+        c: "The third one",
+        d: "The fourth one"
+    },
+    rightanswer: "c"
+}
+]
